05a17a3adf418d1035329199bf2edb981ff00e5d
Ben Clark <benjamin.k.clark@gmail.com>
2016-02-21 22:17:25 -0500

    add update method to server

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
+import json
 import os
 
 from flask import Flask
 from flask import render_template
+from geojson import update_geojson
 
 
 rel_path = os.path.dirname(os.path.abspath(__file__))
@@ -18,6 +20,23 @@ CONFIG = {
 @app.route('/map')
 def index():
     return render_template('map.html', config=CONFIG)
+
+
+@app.route('/update', methods=['POST'])
+def update():
+    try:
+        update_geojson()
+        return (
+            json.dumps({'success': True}),
+            200,
+            {'ContentType': 'application/json'})
+    except Exception as e:
+        return (
+            'Error: {0}'.format(e),
+            500,
+            {'ContentType': 'application/json'}
+        )
+
 
 if __name__ == '__main__':
     app.run()
